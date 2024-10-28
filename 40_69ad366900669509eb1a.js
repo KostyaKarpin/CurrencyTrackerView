6ce34b2ecfbbaa189f2c0827150a1839466d4460
KostyaKarpin <113770374+KostyaKarpin@users.noreply.github.com>
2024-10-28 18:45:50 +0300

    Add files via upload

---
+++ 40_69ad366900669509eb1a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkCurrencyTracker=self.webpackChunkCurrencyTracker||[]).push([[40],{40:(r,e,c)=>{c.r(e),c.d(e,{Contacts:()=>s});var n=c(848),s=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"Contacts Page"})})}}}]);
+//# sourceMappingURL=40_69ad366900669509eb1a.js.map
