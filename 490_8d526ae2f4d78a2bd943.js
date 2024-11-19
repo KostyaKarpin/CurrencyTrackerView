0d941d3c3ef889988f7621456e071a7e4998ced7
KostyaKarpin <113770374+KostyaKarpin@users.noreply.github.com>
2024-11-19 13:50:57 +0300

    Add files via upload

---
+++ 490_8d526ae2f4d78a2bd943.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkCurrencyTracker=self.webpackChunkCurrencyTracker||[]).push([[490],{6490:(e,r,n)=>{n.r(r),n.d(r,{NotFound:()=>u});var c=n(4848),u=function(){return(0,c.jsxs)("div",{"data-testid":"notFound-page",children:[(0,c.jsx)("h1",{children:"404"}),(0,c.jsx)("p",{children:"Page not found"})]})}}}]);
+//# sourceMappingURL=490_8d526ae2f4d78a2bd943.js.map
